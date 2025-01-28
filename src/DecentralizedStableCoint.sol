e7037c62e0ddb16bb3e7e997aa8c8ebc1317d4d3
gkarumbi <gkarumbi@gmail.com>
2025-01-28 11:35:33 +0300

    Add a burn function that overrides the original burn function from the ERC20Contract and takes in the amount to burn, checks if the amount is more than zero and is more than the senders balance and then calles the burn function from the super class and burns the tokens

---
+++ src/DecentralizedStableCoint.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18 ;
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+/* 
+* @title DecentralizedStableCoint
+* @author George Karumbi
+* Collateral: Exogenous (ETH & BTC)
+* Minting: Alogrithmic
+* Relative Stability: Peggeds To USD
+*
+*This is the contract meant to be government by DSCEngine.
+*This contract is just the ERC20 implementation of our stable coint systme
+ */
+
+contract DecentralizedStableCoin  is ERC20Burnable, Ownable {
+
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC"){}
+
+    function burn(uint256 _amount) public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <= 0){
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+
+        }
+        if(balance < _amount){
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+        }
+
+        super.burn(_amount);
+    }
+}
