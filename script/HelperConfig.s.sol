b064e2dc7468cc548b08f601e099a1e2c97a782e
gkarumbi <gkarumbi@gmail.com>
2025-01-30 11:16:04 +0300

    -Add HelperConfig contract to help us manage different configurations needed in our DeployDSC script -Add NetworkConfig Struct to track our token assets and priceFeed addresses' -Add getSepoliaEthConfig thats a struck(object) with configuration for the sepolia network such as the wETH price feed address on the sepolia testnet -Add constant DECIMAL to stand for the price feed prices decimal place, ETH_USD_PRICE & BTC_USD_PRICE to hard code prices in our mocks -Add getOrCreateAnvilEthConfig() that checks that our wethPriceFeed address is not address(0), the starts a broadcast of a MockV3Aggregator Contrack and a mocl ERC20Mock contract for both wETH and wBTC, that simulate a deploy priceFeed and deployed ERC contracts for both wETH and wBTC and then return a network config with config for our local anvil chain -Add constructor, when the HelperConfig contract is deployed, it will first confirm the chainid and the using the chain id set the correct configs based on whether the chainid is for our sepolia testnetwork or our local anvil test network

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+
+contract HelperConfig is Script{}
